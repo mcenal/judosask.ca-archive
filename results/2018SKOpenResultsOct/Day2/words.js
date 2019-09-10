caca60323b2b6fe3710cac324311b33453192a15
Ezra Lalonde <ezra@usefuliftrue.com>
2019-09-10 15:31:00 -0600

    site as it appearnd on 2019 september 10, mirrored from black sun ftp

---
+++ results/2018SKOpenResultsOct/Day2/words.js
@@ -0,0 +1,16 @@
+var txt_not_drawn = "Not drawn";
+var txt_finished = "Finished";
+var txt_match_ongoing = "Match ongoing";
+var txt_started = "Started";
+var txt_drawing_ready = "Drawing ready";
+var txt_coach = "Coach";
+var txt_firstname = "Name";
+var txt_lastname = "Surname";
+var txt_weight = "Weight:";
+var txt_category = "Category";
+var txt_status = "Status";
+var txt_place = "Pos";
+var txt_display = "Display";
+var txt_match_after_1 = "Match after";
+var txt_match_after_2 = "matches";
+var txt_coach_display = "Coach Display";
